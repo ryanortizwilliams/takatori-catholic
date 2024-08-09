4e7b3b5f3996b714605570ad5c8738146dc2ad53
ryanortizwilliams <ryanortizwilliams@outlook.com>
2024-08-09 12:15:38 +0900

    Add navbar,fix formatting

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
 </head>
+
 <body>
-    <h1>Testing home page</h1>
+    <x-layout>
+        <div class="container">
+            <h1>Home page</h1>
+
+        </div>
+    </x-layout>
 </body>
+
 </html>
